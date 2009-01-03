82a3a48b8759f0335105b73204cadbc7ffaf9496
ssmith <ssmith@f4b04360-953d-48bb-9aa4-7cb10f2341d2>
2009-01-03 17:19:29 +0000

    moved test classes from lib/ to t/
    git-svn-id: file:///Users/ssmith/svn-rep/rmi@41 f4b04360-953d-48bb-9aa4-7cb10f2341d2

---
+++ lib/RMI/Server/NamedPipes.pm
@@ -0,0 +1,28 @@
+#!/usr/bin/env perl
+
+package RMI::Server::NamedPipes;
+
+use strict;
+use warnings;
+use base 'RMI::Server';
+use IO::File;     
+
+sub new {
+    my ($class,$reader_path,$writer_path) = (@_); 
+    my $reader = IO::File->new($reader_path);
+    $reader or die "Failed to open reader $reader_path: $!";
+    my $writer = IO::File->new('>' . $writer_path);   
+    $writer or die "Failed to open writer $writer_path: $!";
+    $writer->autoflush(1);
+
+    my $self = $class->SUPER::new(
+        peer_pid => -1,
+        writer => $writer,
+        reader => $reader,
+    );
+
+    return $self;    
+}
+
+1;
+
