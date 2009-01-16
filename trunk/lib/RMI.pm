d1fdb9a5fd1dc2681ae7d6b5d7b56b0f55b5110d
ssmith <ssmith@f4b04360-953d-48bb-9aa4-7cb10f2341d2>
2009-01-16 09:36:05 +0000

    Added build files.  Updated POD with Copyright.
    git-svn-id: file:///Users/ssmith/svn-rep/rmi@93 f4b04360-953d-48bb-9aa4-7cb10f2341d2

--- trunk/lib/RMI.pm
+++ trunk/lib/RMI.pm
@@ -217,6 +217,16 @@ B<IO::Socket>, B<Tie::Handle>, B<Tie::Array>, B<Tie:Hash>, B<Tie::Scalar>
 
 Scott Smith <sakoht@cpan.org>                    
 
+=head1 COPYRIGHT
+    
+Copyright 2008 - 2009 Scott Smith
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+The full text of the license can be found in the LICENSE file included
+with this module.
+
 =cut
 
 1;
