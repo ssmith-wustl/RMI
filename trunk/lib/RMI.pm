b62e2e2ea6dc488cee870f25dbdd1ef13b0df5fe
ssmith <ssmith@f4b04360-953d-48bb-9aa4-7cb10f2341d2>
2009-01-16 09:13:12 +0000

    moved files under trunk
    git-svn-id: file:///Users/ssmith/svn-rep/rmi@85 f4b04360-953d-48bb-9aa4-7cb10f2341d2

---
+++ trunk/lib/RMI.pm
@@ -0,0 +1,223 @@
+package RMI;
+
+use strict;
+use warnings;
+use version;
+our $VERSION = qv('0.1');
+
+# the whole base set of classes which make general RMI work
+# (sub-classes of RMI Server & Client provide specific implementations such as sockets, etc.)
+use RMI::Node;
+use RMI::Client;
+use RMI::Server;
+use RMI::ProxyObject;
+use RMI::ProxyReference;
+
+# turn on debug messages if an environment variable is set
+our $DEBUG;
+BEGIN { $RMI::DEBUG = $ENV{RMI_DEBUG}; };
+
+# this is used at the beginning of each debug message
+# setting it to a single space for a server makes server/client distinction
+# more readable in combined output.
+our $DEBUG_MSG_PREFIX = '';
+
+=pod
+
+=head1 NAME
+
+RMI - (mostly) transparet Remote Method Invocation
+
+=head1 SYNOPSIS
+
+#process 1: an example server on host "myserver"
+
+    my $s = RMI::Server::Tcp->new(port => 1234);
+    $s->run;
+
+
+#process 2: an example client
+
+    my $c = RMI::Client::Tcp->new(
+       host => 'myserver',
+       port => 1234,
+    );
+
+    $c->call_use('IO::File');
+    $o = $c->call_class_method('IO::File','new','/etc/passwd');
+    
+    $line1 = $o->getline;           # works as an object
+
+    $line2 = <$o>;                  # works as a file handle
+    @rest  = <$o>;                  # detects scalar/list context correctly 
+
+    $o->isa('IO::File');            # transparent in standard ways
+    $o->can('getline');             
+    ref($o) eq 'RMI::ProxyObject';  # the only sign this isn't a real IO::File...   
+                                    # (see use_remote() to fix this by full class proxying)    
+    
+=head1 DESCRIPTION
+
+The RMI suite includes RMI::Client and RMI::Server classes.  An RMI::Client module allows an application to generate 
+objects in a remote process which is running an RMI::Server, and use them via a "proxy".  The proxy object behaves
+as though it were the real object/reference, but redirects all interaction to the real object server.
+
+The proxying or objects via a remote stub goes by the term "RMI" in Java, "Remoting" in .NET, and is similar in functionalty
+to architectures such as CORBA, and the older DCOM.
+
+=head1 PROXY OBJECTS AND REFERENCES
+
+Parameters and results for remote method calls (and also plain subroutine calls, and call_eval() calls) are passed
+as transparent proxies when they are references of any sort.  This includes objects, and also HASH refrences, ARRAY
+references, SCALAR references, GLOBs/IO-handles, and CODE references, including closures.  Proxy objects are also
+usable as their primitive Perl type, in addition to dispatching method calls.  
+
+As such there is NO "SERIALIZATION" of data structures.  When a method returns a hashref, the client gets something
+which looks and acts like a hashref, but access to it results in activity across the client-server connection.
+
+Objects and references keep state in the process in which they originate.  Only parameters and return values which
+are non-reference values are passed to the other side by copy.
+
+When a parameter is a reference, the sender keeps a link to the object in question for the receiver, and sends
+the receiver an ID for the item.  The receiver produces a proxy reference, which calls back to the sender for
+all attempts to interact with it.  Upon destruction on the reciever side, a message is sent to the sender to expire
+its link to the item in question, and allow garbage collection if no other references exist.
+
+=head1 TYPES OF CLIENTS AND SERVERS
+
+All RMI client and server objects use a pair of handles for messaging.  Specific subclasses of RMI::Client
+and RMI::Server implement the handles in different ways.  There are two implementations which are part
+of the default RMI package:
+
+=over 4
+
+=item RMI::Client::Tcp and RMI::Server::Tcp
+
+A TCP/IP socket server for cross-network proxies.  The current implementation supports multiple clients,
+and is a single-threaded non-blocking server.
+
+=item RMI::Client::ForkedPipes and RMI::Server::ForkedPipes
+
+Creates its own private server in a sub-process.  Useful if you want an out-of-process object b/c you
+plan to exceed Perl's memory limit on a 32-bit machine, or for testing w/o making a socket.
+
+(This is also used by custom server apps since the server will exec() whatever was passed to the
+client constructor after fork().  In particular, it was built to allow cross-langage RMI.)
+
+=back
+
+=head1 METHODS
+
+The RMI module has no public methods of its own.  See <RMI::Client> and <RMI::Server> for APIs for interaction.
+
+=head1 DEBUGGING RMI CODE
+
+The environment variable RMI_DEBUG, has its value transferred to $RMI::DEBUG
+at compile time.  When set to 1, this will cause the RMI modules to emit detailed
+information to STDERR during all "conversations" between itself and the remote
+side. This works for RMI::Client, RMI::Server, and anything else which
+inherits from RMI::Node.
+
+for example, using bash to run the first test case:
+RMI_DEBUG=1 perl -I lib t/01_*.t
+
+The package variable $RMI::DEBUG_MSG_PREFIX will be printed at the beginning of each message.
+Changing this value allows the viewer to separate both halves of a conversation.
+The test suite sets this value to ' ' for the server side, causing server activity
+to be indented.
+
+=head1 FUNCTIONALITY CAVEATS
+
+=over 2
+
+=item Anything which relies on caller() to check the call stack may not work as intended.
+
+  This means that some modules which perform magic during import() may not work as intended.
+
+=item Proxied objects/references reveal that they are proxies when ref($o) is called on them, unless the entire package is proxied with ->use_remote.
+
+  There is no way to override this, as far as I know.
+
+=item No inherent security is built-in.
+
+ Writing a wrapper for an RMI::Server which limits the calls it supports, and the data
+ returnable would be easy, but it has not been done.  Specifically, turning off
+ call_eval() is wise in untrusted environments.
+
+=item Calls to "use_remote" will proxy subroutine calls, but not package variable access automatically.
+
+
+  Also implementable, but this does not happen automatically.  Perhaps it should for @ISA?
+  
+  $c->use_remote("Some::Package");
+  # $Some::Package::foo is NOT bound to the remote variable of the same name
+  
+  *Some::Package::foo = $c->call_eval('\\$Some::Package::foo');
+  # now it is...
+
+=back
+
+=head1 BUGS
+
+=over 2
+
+=item The client may not be able to "tie" variables which are proxies.
+
+  The RMI modules use "tie" on every proxy reference to channel access to the other side.
+  The effect of attempting to tie a proxy reference may destroy its ability to proxy.
+  (This is untested.)
+  
+  In most cases, code does not tie a variable created elsewhere, because it destroys its prior value,
+  so this is unlikely to be an issue.
+
+=item Remote calls to subroutines/methods which modify $_[$n] directly to tamper with the caller's variables will fail.
+
+  This is supportable, but adds considerable overhead to support modules which create a
+  side effect which is avoided because it is, mostly, a bad idea.
+
+  Perl technically passes an alias to even non-reference values, though the common "my ($v1,$v2) = @_;"
+  makes a copy which safely allows the subroutine to behave as though the values were pass-by-copy.
+  
+  sub foo {
+    $_[0]++; # BAD!
+  }
+  my $v = 1;
+  foo($v);
+  $v == 2; # SURPRISE!
+
+  If foo() were called via RMI, in the current implementation, $v would still have its original value.
+  
+  Packages which implement this surprise behavior include Compress::Zlib!  If this feature were added
+  the overhead to Compress::Zlib would still make you want to wrap the call...
+
+=item The serialization mechanism needs to be made more robust and efficient.
+
+ It's really just enough to "work".
+
+ The current implementation uses Data::Dumper with options which should remove newlines.
+ Since we do not flatten arbitrary data structures, a simpler parser would be more efficient.
+
+ The message type is currently a text string.  This could be made smaller.
+
+ The data type before each paramter or return value is an integer, which could also
+ be abbreviated futher, or we could go the other way and be more clear. :)
+
+ We should switch to sysread and pass the message length instead of relying on buffers,
+ since the non-blocking IO might not have issues.
+
+=back
+
+=head1 SEE ALSO
+
+B<RMI::Server> B<RMI::Client>, B<RMI::Node>
+
+B<IO::Socket>, B<Tie::Handle>, B<Tie::Array>, B<Tie:Hash>, B<Tie::Scalar>
+
+=head1 AUTHOR
+
+Scott Smith <sakoht@cpan.org>                    
+
+=cut
+
+1;
+
