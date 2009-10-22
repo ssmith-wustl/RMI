6bdbcbc8dd5503d657c326a623c263a4a52dea64
ssmith <ssmith@Scotts-Laptop.local>
2009-10-21 23:12:35 -0500

    updated version number

--- perl/lib/RMI.pm
+++ perl/lib/RMI.pm
@@ -3,7 +3,7 @@ package RMI;
 use strict;
 use warnings;
 use version;
-our $VERSION = qv('0.06');
+our $VERSION = qv('0.07');
 
 # the whole base set of classes which make general RMI work
 # (sub-classes of RMI Server & Client provide specific implementations such as sockets, etc.)
