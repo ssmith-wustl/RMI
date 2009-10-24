b6a97e12257505f178e7aab492a12dd9cad7dc86
ssmith <ssmith@Scotts-Laptop.local>
2009-10-23 19:36:16 -0500

    Build for v0.08 release.

--- perl5/lib/RMI.pm
+++ perl5/lib/RMI.pm
@@ -3,7 +3,7 @@ package RMI;
 use strict;
 use warnings;
 use version;
-our $VERSION = qv('0.07');
+our $VERSION = qv('0.08');
 
 # the whole base set of classes which make general RMI work
 # (sub-classes of RMI Server & Client provide specific implementations such as sockets, etc.)
@@ -33,7 +33,7 @@ RMI - Remote Method Invocation with transparent proxies v0.07
 
 head1 VERSION
 
-This document describes RMI v0.07.
+This document describes RMI v0.08.
 
 =head1 SYNOPSIS
 
