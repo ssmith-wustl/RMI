327c4a2bfca365924058b6503b2a757fdf2b9bcb
Scott Smith <sakoht@cpan.org>
2010-10-24 17:46:41 -0500

    removed old prove script, updated comments

--- perl5/lib/RMI/Proxy/DBI/db.pm
+++ perl5/lib/RMI/Proxy/DBI/db.pm
@@ -6,7 +6,7 @@ package RMI::Proxy::DBI::db;
 # and then the Perl implementation is called if the optional attributes hash is present with a "Slice" key.
 # For some reason, when this hashref is a proxy, errors occur on the C side, and we never make it back to Perl.
 
-# It's safe to use a proxy here, because DBI::db never attempts to mutlate the hash in a way the caller expects
+# It's safe to use a copy here, because DBI::db never attempts to mutlate the hash in a way the caller expects
 # to observe later.
 
 $RMI::ProxyObject::DEFAULT_OPTS{"DBI::db"}{"selectall_arrayref"} = { copy => 1 };
