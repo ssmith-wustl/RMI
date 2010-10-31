9a3ad3de5876ab4e73752aec62f7aef30dfec68a
Scott Smith <sakoht@cpan.org>
2010-10-31 02:15:34 -0500

    updated the version to 0.11 in the final location requiring it

--- perl5/lib/RMI.pm
+++ perl5/lib/RMI.pm
@@ -2,7 +2,7 @@ package RMI;
 
 use strict;
 use warnings;
-our $VERSION = '0.10';
+our $VERSION = '0.11';
 
 # the whole base set of classes which make general RMI work
 # (sub-classes of RMI Server & Client provide specific implementations such as sockets, etc.)
